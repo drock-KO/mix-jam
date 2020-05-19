bc6b3da2c9eb5eaf47ca51f0a3a8dd17da2d2d8b
drock-KO <kazuma.okada@di-v.co.jp>
2020-05-19 21:16:46 +0900

    modified indent 4 => 2

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,9 +1,9 @@
 class Track < ApplicationRecord
 
-    belongs_to :user
+	belongs_to :user
 
-    has_many :liked_users, through: :likes, source: :user
-   
-    has_many :tags, through: :track_tags
-    has_many :track_tags
+	has_many :liked_users, through: :likes, source: :user
+
+	has_many :tags, through: :track_tags
+	has_many :track_tags
 end
