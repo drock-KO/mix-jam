0705ce87208369493e0f0d6a28cc1e22e8725e8f
drock-KO <kazuma.okada@di-v.co.jp>
2020-05-19 21:22:25 +0900

    modified priority of association from top to bottom in track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,9 +1,7 @@
 class Track < ApplicationRecord
+	has_many :liked_users, through: :likes, source: :user
+	has_many :tags, through: :track_tags
 
 	belongs_to :user
-
-	has_many :liked_users, through: :likes, source: :user
-
-	has_many :tags, through: :track_tags
 	has_many :track_tags
 end
