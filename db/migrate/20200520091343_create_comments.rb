ee76c3250e554c88158716ea9c2eab3d0a0ca4da
drock-KO <kazuma.okada@di-v.co.jp>
2020-05-20 18:27:47 +0900

    reset comment table and model

--- db/migrate/20200520091343_create_comments.rb
+++ db/migrate/20200520091343_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.text :text, null: false
+      t.string :text, null: false
       t.references :track, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
